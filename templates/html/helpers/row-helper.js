081b8c1982f08371c438dc9d288c27e0b300ea28
Tim <timothy@overly.me>
2017-05-13 21:51:26 -0400

    Tweak the large layout and update some skills

--- templates/html/helpers/row-helper.js
+++ templates/html/helpers/row-helper.js
@@ -1,6 +1,6 @@
 module.exports.register = function(Handlebars, options, params) {
   Handlebars.registerHelper('row', function(value, options) {
-    if((value)%4 == 0){
+    if((value)%3 == 0){
        return options.fn(this);
     } else {
        return options.inverse(this);
